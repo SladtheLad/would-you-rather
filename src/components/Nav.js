23e6c05b02eb4a2029045dc9ecdc1320d08fc993
Nick Sladden <nickoliasxii@gmail.com>
2018-11-06 17:25:56 -0600

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,22 +5,21 @@ import { Link } from 'react-router-dom'
 class Nav extends Component {
   render() {
     return (
-      <div class="nav">
-        <ul>
-          <Link to={'/home'} className="nav-item">
-            HOME
-          </Link>
-          <Link to={'/add'} className="nav-item">
-            NEW QUESTION
-          </Link>
-          <Link to={'/leaderboard'} className="nav-item">
-            LEADERBOARD
-          </Link>
-          <Link to={'/'} className="nav-item">
-            LOGOUT
-          </Link>
-        </ul>
-      </div>
+      <nav class="nav nav-tabs nav-fill">
+        <Link to={'/home'} className="nav-item nav-link">
+          HOME
+        </Link>
+        <Link to={'/add'} className="nav-item nav-link">
+          NEW QUESTION
+        </Link>
+        <Link to={'/leaderboard'} className="nav-item nav-link">
+          LEADERBOARD
+        </Link>
+        <span>Hello USER</span>
+        <Link to={'/'} className="nav-item nav-link">
+          LOGOUT
+        </Link>
+      </nav>
     )
   }
 }
